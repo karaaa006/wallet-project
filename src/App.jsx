5449c579e9669071b01ab018832e0f1ed7ad016b
NelliBilovol <nelli199020@gmail.com>
2022-04-04 17:11:42 +0300

    initial chart

--- src/App.jsx
+++ src/App.jsx
@@ -12,6 +12,7 @@ import { useDispatch, useSelector } from "react-redux";
 import { useEffect } from "react";
 import { apiTokenConfig } from "./api/api";
 import { fetchCurrentUser } from "./redux/operations/userOperations";
+import { Chart } from "./components/Chart";
 
 const GlobalStyle = createGlobalStyle`
   *, *::after, *::before{
@@ -67,9 +68,13 @@ function App() {
         <Route
           path="dashboard"
           element={
-            <PrivateRoute>
+            <>
               <DashboardPage />
-            </PrivateRoute>
+              <Chart />
+            </>
+            // <PrivateRoute>
+
+            // </PrivateRoute>
           }
         />
       </Routes>
