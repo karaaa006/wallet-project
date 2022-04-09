95c28bc98f389fe048eb9bae462199d70bdc63e0
Oleh <olegkaraaa@gmail.com>
2022-04-09 20:51:17 +0300

    media queries var added

--- src/utils/stylesVars.js
+++ src/utils/stylesVars.js
@@ -8,3 +8,8 @@ export const secondCl = "#4A56E2";
 export const negativeCl = "#FF6596";
 export const backdropCl = "rgba(0, 0, 0, 0.25)";
 export const mainBgCl = "#E5E5E";
+
+export const size = {
+  M: "@media screen and (max-width: 768px)",
+  L: "media screen and (max-width: 1280px)",
+};
