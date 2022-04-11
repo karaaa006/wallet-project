3709494cdd2eb3deac99b6927101042157627641
AleksandrOnis <sashaonis@gmail.com>
2022-04-11 18:31:23 +0300

    add validation

--- src/components/Dashboard/AddTransaction/AddTransaction.jsx
+++ src/components/Dashboard/AddTransaction/AddTransaction.jsx
@@ -7,22 +7,11 @@ import { Button } from "../../Button";
 import { ButtonsWrap } from "../../ButtonsWrap";
 import { Input } from "../../Input";
 import { Toggle } from "./Toggle";
-import { Select } from "./Select";
 import iconDate from '../../../images/icons/date.svg'
-import iconSelect from '../../../images/icons/select.svg'
 import { addTransaction } from '../../../redux/operations/financeOperations'
 import { api } from "../../../api/api";
 import { DropDown } from "../../DropDown";
-
-const SchemaYup = Yup.object({
-  // selected: Yup.number('Выберите категорию').required('Выберите категорию'),
-  amount: Yup.number('Введите сумму').required('Введите сумму'),
-  comment: Yup.string('Коментарий')
-    .max(10, 'Ваш комментарий слишком велик')
-});
-
-const Calendar = styled(Input)`
-`
+import { toast } from "react-toastify";
 
 const InputWrap = styled.div`
     display: flex;
@@ -30,20 +19,11 @@ const InputWrap = styled.div`
     align-items: center;
 `;
 
-const SelectorIcon = styled.img`
-position: absolute;
-pointer-events: none;
-top: 196px;
-left: 422px;
-z-index: 0;
-cursor: pointer;
-`;
-
 const DateIcon = styled.img`
 pointer-events: none;
 position: absolute;
-top: 260px;
-left: 412px;
+top: 360px;
+left: 367px;
 background-color: #fff;
 cursor: pointer;
 `;
@@ -55,10 +35,7 @@ const optionsExpense = [];
 let localDate = new Date().toISOString().split('T')[0];
 
 export const AddTransaction = ({modalIsOpen, closeModal}) => {
-    const [amount, setAmount] = useState();
-    const [comment, setComment] = useState("");
-    const [date, setDate] = useState(localDate);
-    const [selected, setSelected] = useState(defaultValueSelected);
+    const [selectedCategory, setSelectedCategory] = useState(defaultValueSelected);
     const [toggle, setToggle] = useState(true);
     const [categories, setCategories] = useState([]);
 
@@ -66,28 +43,12 @@ export const AddTransaction = ({modalIsOpen, closeModal}) => {
 
     const onChangeToggle = () => {
       setToggle(!toggle);
-      setSelected(defaultValueSelected);
+      setSelectedCategory(defaultValueSelected);
     };
-    const onChangeSelected = (event) => setSelected(event.target.value)
-    
-    const onSubmit = event => {
-      event.preventDefault();
-      console.log(newTransaction);
-      dispatch(addTransaction(newTransaction));
-      closeModal();
-    };
-
-    const newTransaction = {
-      "isExpense": toggle,
-      "category": selected,
-      "amount": Number.parseFloat(amount),
-      comment,
-    }
 
     useEffect(() => {
       const getCategories = async () => {
         const data = await api.categories.getCategories()
-        console.log("add", data);
         setCategories([...data]);
       };
       if (categories.length===0) getCategories();
@@ -103,82 +64,100 @@ export const AddTransaction = ({modalIsOpen, closeModal}) => {
           : optionsIncome.push(categories[i])
         }
     }, [categories]);
+ 
 
+    const notify = (text) => toast.warn(text);
+
+    const validationSchema = Yup.object().shape(
+      {
+        amount: Yup.number().required(() => notify('Введите сумму'))
+          .min(0.01, () => notify('Сумма должна быть больше 0')),
+        comment: Yup.string()
+          .max(500, () => notify('Ваш комментарий слишком велик')),
+        date: Yup.date().required(() => notify('Выберите дату'))
+          .max(localDate, () => notify('Выбранная дата ещё не наступила')),
+      }
+    );
 
     return (
         <>
         <Toggle active={toggle} onChange={onChangeToggle} />
-        <Select 
-          defaultValue={defaultValueSelected}
+        <DropDown
           options={toggle ? optionsExpense : optionsIncome} 
-          categories={categories} 
-          onChange={onChangeSelected} 
-          required
-        /> 
-        <SelectorIcon src={iconSelect}/>
-        {/* <DropDown
-          options={toggle ? optionsExpense : optionsIncome} 
-          selectedOption={selected}
-          setSelectedOption={setSelected}
+          selectedOption={selectedCategory}
+          setSelectedOption={setSelectedCategory}
           placeholder="Выберите категорию"
           underline
-        /> */}
-        
-        <form onSubmit={onSubmit}>
-        <InputWrap>
-          <Input
-          placeholder="0.00"
-          w="181px"
-          mb="40px"
-          type="number"
-          name="amount"
-          value={amount}
-          setValue={setAmount}
-          required
-          />
-
-          <Calendar
-          w="181px"
-          mb="40px"
-          type="date"
-          name="date"
-          value={date}
-          setValue={setDate}
-          required
-          /> 
-          <DateIcon src={iconDate}/>
-
-        </InputWrap>
-        <Input
-          placeholder="Комментарий"
-          mb="40px"
-          type="text"
-          name="comment"
-          value={comment}
-          setValue={setComment}
+          m="0 0 40px 0"
         />
-        <ButtonsWrap>
-          <Button
-            type="submit"
-            accent
-            w="100%"
-            mw="300px"
-            h="50px"
-            m="0 0 20px 0"
-            p="0"
+        <Formik
+          initialValues={{ amount: '', comment: '', date: localDate }}
+          validateOnChange
+          onSubmit={(values) => {
+            const newTransaction = {amount: values.amount, comment: values.comment, category: selectedCategory._id, "isExpense": selectedCategory.isExpense}
+            dispatch(addTransaction(newTransaction));
+            // console.log(newTransaction);
+          }}
+          validationSchema={validationSchema}
           >
-            ДОБАВИТЬ
-          </Button>
-          <Button
-            w="100%"
-            mw="300px"
-            h="50px"
-            onClick={closeModal}
-          >
-            ОТМЕНА
-          </Button>
-        </ButtonsWrap>        
-        </form>
-      </>
+            {({values, errors, touched, handleChange, handleBlur, isValid, handleSubmit, handleReset, dirty}) => (
+              <>
+                  <Input
+                  type="number"
+                  name="amount"
+                  placeholder="0.00"
+                  value={values.amount}
+                  onChange={handleChange}
+                  onBlur={handleBlur}
+                  mb="40px"
+                  />
+
+                  <Input
+                  type="date"
+                  name="date"
+                  value={values.date}
+                  onChange={handleChange}
+                  onBlur={handleBlur}
+                  mb="40px"
+                  />
+
+                  <DateIcon src={iconDate}/>
+
+                  <Input
+                  type="text"
+                  name="comment"
+                  placeholder="Комментарий"
+                  value={values.comment}
+                  onChange={handleChange}
+                  onBlur={handleBlur}
+                  mb="40px"
+                  />
+
+                  <ButtonsWrap>
+                    <Button
+                      type="submit"
+                      accent
+                      w="100%"
+                      mw="300px"
+                      h="50px"
+                      m="0 0 20px 0"
+                      p="0"
+                      onClick={handleSubmit}
+                    >
+                      ДОБАВИТЬ
+                    </Button> 
+                    <Button
+                      w="100%"
+                      mw="300px"
+                      h="50px"
+                      onClick={closeModal}
+                    >
+                      ОТМЕНА
+                    </Button>
+                  </ButtonsWrap>  
+            </>
+            )}
+        </Formik>
+        </>
     )
 }
