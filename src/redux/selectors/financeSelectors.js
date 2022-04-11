bf68cb1baa67c9fa73026098f0bae670214ad8ff
Oleh <olegkaraaa@gmail.com>
2022-04-11 18:22:54 +0300

    fix balance

--- src/redux/selectors/financeSelectors.js
+++ src/redux/selectors/financeSelectors.js
@@ -6,7 +6,7 @@ const getFinanceError = (state) => state.finance.error;
 const getBalance = (state) => {
   if (state?.finance?.financeData?.length > 0) {
     const transactions = getFinance(state);
-    const lastTransaction = transactions[transactions.length - 1];
+    const lastTransaction = transactions[0];
     const balance = lastTransaction.balance;
     return balance;
   }
